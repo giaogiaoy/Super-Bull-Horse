b79e63051ad7ce574cc2ba5fa644a55406a6f707
Donglinjie594 <1622497478@qq.com>
2025-04-02 11:39:08 +0800

    docs: 添加路由文件夹

--- projectVue/src/views/Layout/LayoutView.vue
+++ projectVue/src/views/Layout/LayoutView.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import { reactive, ref } from 'vue'
-const state = reactive({
-  count: 0,
-})
+// const state = reactive({
+//   count: 0,
+// })
 
 const count = ref(0)
 </script>
 
 <template>
   <div>
-    <p>raest,{{ state.count }}{{ count }}</p>
+    <p>raest</p>
   </div>
 </template>
 
